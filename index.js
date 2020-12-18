b76a59099420e1335f89adf09239684e9371ad96
José Lastra <jose.lastra@23people.io>
2020-12-18 00:17:10 -0300

    feat: get details from every fighter in the ufc

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const puppeteer = require("puppeteer");
 
 (async () => {
   let browser;
+  const baseUrl = "https://www.ufc.com/";
   try {
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({ headless: false, slowMo: 250 });
     const page = await browser.newPage();
-    await page.goto("https://www.ufc.com/", {
+    await page.goto(baseUrl, {
       waitUntil: "networkidle2",
     });
 
@@ -36,10 +37,49 @@ const puppeteer = require("puppeteer");
         (x) => x.textContent
       );
       console.log(divisionName);
-      const fighters = await column.$$eval('a[href^="/athlete/"]', (nodes) =>
-        nodes.map((n) => n.innerHTML)
+      const fightersUrl = await column.$$eval('a[href^="/athlete/"]', (nodes) =>
+        nodes.map((n) => n.getAttribute("href"))
       );
-      console.log(fighters);
+      for (const fighterUrl of fightersUrl) {
+        let fighterPage = await browser.newPage();
+        await fighterPage.goto(
+          `${baseUrl.slice(0, baseUrl.length - 1)}${fighterUrl}`,
+          { waitUntil: "networkidle2" }
+        );
+        await fighterPage.waitForSelector(".c-hero__headline-suffix", {
+          timeout: 4000,
+        });
+        await fighterPage
+          .waitForSelector(".field-name-nickname", {
+            timeout: 4000,
+          })
+          .catch((e) =>
+            console.log(
+              `This fighter does not have a nickname. \nError: ${e.message}`
+            )
+          );
+        let nickname = await fighterPage
+          .$eval(".field-name-nickname", (div) => div.textContent)
+          .catch(() => "");
+        await fighterPage.waitForSelector(".field-name-name", {
+          timeout: 4000,
+        });
+        let fullname = await fighterPage.$eval(
+          ".field-name-name",
+          (div) => div.textContent
+        );
+        await fighterPage.waitForSelector(".c-hero__headline-suffix", {
+          timeout: 4000,
+        });
+        let description = await fighterPage.$eval(
+          ".c-hero__headline-suffix",
+          (div) => div.textContent
+        );
+        console.log("nickname", nickname);
+        console.log("fullname", fullname);
+        console.log("description", description);
+        await fighterPage.close();
+      }
     }
 
     await browser.close();

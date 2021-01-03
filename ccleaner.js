048c482540d0ecd439971dc9f303db2a6925fc98
José Lastra <lastrafda@gmail.com>
2021-01-03 00:19:12 -0300

    clean stats and start using Prototypal inheritance

--- ccleaner.js
+++ ccleaner.js
@@ -1,38 +1,26 @@
 const cleaner = (rankings) => {
   const holder = [];
-  const i = 0;
   rankings.forEach((rank) => {
     holder.push({
       division: cleanString(rank.division),
       fighters: rank.fighters.map((fighter) => {
-        // I can't give it a specific name because itsn't fixed, deal with it...
-        let [firstPromo, secondPromo, thirdPromo] = fighter.promoted;
-        // lets add the "promoted" section
-        // @TODO: refactor this big boi into smaller functions
-        return {
-          nickname: cleanString(fighter.nickname),
-          fullname: cleanString(fighter.fullname),
-          description: cleanString(fighter.description),
-          promoted: [
-            {
-              label: cleanString(firstPromo.label),
-              value: parseInt(firstPromo.value, 10),
-            },
-            {
-              label: cleanString(secondPromo.label),
-              value: parseInt(secondPromo.value, 10),
-            },
-            {
-              label: cleanString(thirdPromo.label),
-              value: parseInt(thirdPromo.value, 10),
-            },
-          ],
-        };
+        return fighter.setDescription().setPromotedInfo().setStats();
       }),
     });
   });
   return holder;
 };
+// how can I make it so I can use functions like this ?
+// fighters.map(f => f.setDescription().setPromo().setStats().setBlah())
+// that would be beautiful...
+// const setDescription = (fighter) => {
+//   return {
+//     ...fighter,
+//     nickname: cleanString(fighter.nickname),
+//     fullname: cleanString(fighter.fullname),
+//     description: cleanString(fighter.description),
+//   };
+// };
 
 const cleanString = (text) => {
   if (typeof text === "undefined") {
@@ -41,4 +29,91 @@ const cleanString = (text) => {
   return text.replace(/\"/g, "").replace(/\s+/g, " ").trim();
 };
 
-module.exports = { cleaner, cleanString };
+const parseValueAndPercentageText = (text) => {
+  [value, percentage] = text.split(" ");
+  return {
+    value: parseInt(value, 10),
+    percentage: percentage.replace(/[()]/g, ""),
+  };
+};
+
+if (!Object.prototype.setDescription) {
+  Object.prototype.setDescription = function () {
+    return {
+      ...this,
+      nickname: cleanString(this.nickname),
+      fullname: cleanString(this.fullname),
+      description: cleanString(this.description),
+    };
+  };
+}
+
+if (!Object.prototype.setPromotedInfo) {
+  Object.prototype.setPromotedInfo = function () {
+    const promotedInfo = [...this.promoted];
+    let cleanedPromo = [];
+    for (const promo of promotedInfo) {
+      cleanedPromo.push({
+        label: cleanString(promo.label),
+        value: parseInt(promo.value, 10),
+      });
+    }
+    return {
+      ...this,
+      promoted: cleanedPromo,
+    };
+  };
+}
+
+Object.prototype.setStats = function () {
+  const stats = { ...this.stats };
+  return {
+    ...this,
+    stats: {
+      sigStrikesAttempted: parseInt(stats.sigStrikesAttempted, 10),
+      sigStrikesLanded: parseInt(stats.sigStrikesLanded, 10),
+      takedownsAttempted: parseInt(stats.takedownsAttempted, 10),
+      takedownsLanded: parseInt(stats.takedownsLanded, 10),
+      sigStrikesLandedPerMin: Number(cleanString(stats.sigStrikesLandedPerMin)),
+      sigStrikesAbsorbedPerMin: Number(
+        cleanString(stats.sigStrikesAbsorbedPerMin)
+      ),
+      takedownAvgPerFifteen: Number(cleanString(stats.takedownAvgPerFifteen)),
+      submissionAvgPerFifteen: Number(
+        cleanString(stats.submissionAvgPerFifteen)
+      ),
+      sigStrikeDefence: cleanString(stats.sigStrikeDefence).replace(/\s/, ""),
+      takedownDefence: cleanString(stats.takedownDefence).replace(/\s/, ""),
+      knockdownRatio: Number(cleanString(stats.knockdownRatio)),
+      avgFightTime: cleanString(stats.avgFightTime),
+      sigStrikesByPosition: {
+        standing: parseValueAndPercentageText(
+          stats.sigStrikesByPosition.standing
+        ),
+        clinch: parseValueAndPercentageText(stats.sigStrikesByPosition.clinch),
+        ground: parseValueAndPercentageText(stats.sigStrikesByPosition.ground),
+      },
+      sigStrikeByTarget: {
+        head: {
+          value: parseInt(stats.sigStrikeByTarget.head.value, 10),
+          percentage: stats.sigStrikeByTarget.head.percentage,
+        },
+        body: {
+          value: parseInt(stats.sigStrikeByTarget.body.value, 10),
+          percentage: stats.sigStrikeByTarget.body.percentage,
+        },
+        leg: {
+          value: parseInt(stats.sigStrikeByTarget.leg.value, 10),
+          percentage: stats.sigStrikeByTarget.leg.percentage,
+        },
+      },
+      winByWay: {
+        koTko: parseValueAndPercentageText(stats.winByWay.koTko),
+        dec: parseValueAndPercentageText(stats.winByWay.dec),
+        sub: parseValueAndPercentageText(stats.winByWay.sub),
+      },
+    },
+  };
+};
+
+module.exports = { cleaner, cleanString, parseValueAndPercentageText };
--- test/ccleaner.test.js
+++ test/ccleaner.test.js
@@ -1,11 +1,17 @@
-const { cleaner, cleanString } = require("../ccleaner");
+const {
+  cleaner,
+  cleanString,
+  parseValueAndPercentageText,
+} = require("../ccleaner");
 const { data } = require("../output.json");
 const menp4p = require("./data/menp4p.json");
 
 let khabib;
 let cleanedMen4p4;
+let cleanedKhabib;
 beforeEach(() => {
   [cleanedMen4p4] = cleaner(data);
+  cleanedKhabib = cleanedMen4p4.fighters[0];
   [khabib] = menp4p.fighters;
 });
 
@@ -14,15 +20,13 @@ describe("Cleaner Ranks", () => {
     expect(cleanedMen4p4.division).toBe(menp4p.division);
   });
 
-  test("it can clean the fighters description", () => {
-    let cleanedKhabib = cleanedMen4p4.fighters[0];
+  test('it can clean the fighters "description" section', () => {
     expect(cleanedKhabib.nickname).toEqual(khabib.nickname);
     expect(cleanedKhabib.fullname).toEqual(khabib.fullname);
     expect(cleanedKhabib.description).toEqual(khabib.description);
   });
 
   test('it can clean the fighters "promoted" section', () => {
-    let cleanedKhabib = cleanedMen4p4.fighters[0];
     [
       expectedWinStreak,
       expectedWinsByKnockout,
@@ -33,10 +37,68 @@ describe("Cleaner Ranks", () => {
     expect(winsByKnockout).toEqual(expectedWinsByKnockout);
     expect(winsBySubmission).toEqual(expectedWinsBySubmission);
   });
+
+  test('it can clean the fighters "stats" section', () => {
+    const {
+      sigStrikesAttempted,
+      sigStrikesLanded,
+      takedownsAttempted,
+      takedownsLanded,
+      sigStrikesLandedPerMin,
+      sigStrikesAbsorbedPerMin,
+      takedownAvgPerFifteen,
+      submissionAvgPerFifteen,
+      sigStrikeDefence,
+      takedownDefence,
+      knockdownRatio,
+      avgFightTime,
+      sigStrikesByPosition,
+      sigStrikeByTarget,
+      winByWay,
+    } = khabib.stats;
+    expect(cleanedKhabib.stats.sigStrikesAttempted).toEqual(
+      sigStrikesAttempted
+    );
+    expect(cleanedKhabib.stats.sigStrikesLanded).toEqual(sigStrikesLanded);
+    expect(cleanedKhabib.stats.takedownsAttempted).toEqual(takedownsAttempted);
+    expect(cleanedKhabib.stats.takedownsLanded).toEqual(takedownsLanded);
+    expect(cleanedKhabib.stats.sigStrikesLandedPerMin).toEqual(
+      sigStrikesLandedPerMin
+    );
+    expect(cleanedKhabib.stats.sigStrikesAbsorbedPerMin).toEqual(
+      sigStrikesAbsorbedPerMin
+    );
+    expect(cleanedKhabib.stats.takedownAvgPerFifteen).toEqual(
+      takedownAvgPerFifteen
+    );
+    expect(cleanedKhabib.stats.submissionAvgPerFifteen).toEqual(
+      submissionAvgPerFifteen
+    );
+    expect(cleanedKhabib.stats.sigStrikeDefence).toBe(sigStrikeDefence);
+    expect(cleanedKhabib.stats.takedownDefence).toBe(takedownDefence);
+    expect(cleanedKhabib.stats.knockdownRatio).toEqual(knockdownRatio);
+    expect(cleanedKhabib.stats.avgFightTime).toBe(avgFightTime);
+    expect(cleanedKhabib.stats.sigStrikesByPosition).toMatchObject(
+      sigStrikesByPosition
+    );
+    expect(cleanedKhabib.stats.sigStrikeByTarget).toMatchObject(
+      sigStrikeByTarget
+    );
+    expect(cleanedKhabib.stats.winByWay).toMatchObject(winByWay);
+  });
 });
 
 describe("General Strings cleaner", () => {
   test("it can remove \\n and trim a string", () => {
     expect(cleanString('\n  \n      "The Eagle"\n  ')).toBe("The Eagle");
+  });
+});
+
+describe("Percentages and Values cleaner", () => {
+  test('it can generate a {value,percentage} object from a "XX (X%)" string', () => {
+    expect(parseValueAndPercentageText("263 (40%)")).toMatchObject({
+      value: 263,
+      percentage: "40%",
+    });
   });
 });
